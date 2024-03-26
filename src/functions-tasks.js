a1966b98832fed2bf60a5a7194953b29d3fcde8e
annaAzh <forarre@yandex.ru>
2024-03-27 00:47:42 +0300

    feat: solve getArgumentsCount task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -53,8 +53,9 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const arr = funcs;
+  return arr.map((_, i) => i);
 }
 
 /**
