3031fcf27aeb2246f3d59ed48444a82ac26939f8
annaAzh <forarre@yandex.ru>
2024-03-27 01:02:22 +0300

    feat: solve getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -193,8 +193,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom - 1;
+  function counterFunc() {
+    counter += 1;
+    return counter;
+  }
+  return counterFunc;
 }
 
 module.exports = {
