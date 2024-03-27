f9da46d7467466b427d6abd658f5b0e5380badea
annaAzh <forarre@yandex.ru>
2024-03-27 10:32:56 +0300

    feat: solve setCache task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -109,8 +109,17 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+
+  return function setCache(...args) {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
+    }
+    cache[key] = func.apply(this, args);
+    return cache[key];
+  };
 }
 
 /**
